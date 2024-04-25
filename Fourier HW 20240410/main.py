a46fa23921a6d0437cea59416d366469742345cf
Danny Ho <113899395+DannyHo1209@users.noreply.github.com>
2024-04-25 22:20:21 +0800

    HW20240410

--- Fourier HW 20240410/main.py
+++ Fourier HW 20240410/main.py
@@ -14,23 +14,27 @@ def dft(arr):
 
 
 decimal = 2
-x = np.array([1, 2, 4, 3, 2, 1, 1])
+f = np.array([1, 2, 4, 3, 2, 1, 1])
 g = np.array([1, 1, 1, 1, 1, 1, 1]) * (1/7)
-ftx = dft(x)
+ftf = dft(f)
 ftg = dft(g)
-print('x的傅立葉轉換：')
-print(np.around(ftx, decimal), '\n')
+print('f的傅立葉轉換：')
+print(np.around(ftf, decimal), '\n')
 print('g的傅立葉轉換：')
 print(np.around(ftg, decimal), '\n')
 
-convolve_result = np.convolve(x, g, mode='full')
-print("(a) 卷積的結果 x*g:")
+convolve_result = np.convolve(f, g, mode='full')
+print("(a) 卷積的結果 f*g:")
 print(np.around(convolve_result, decimal), '\n')
 
 ft_result = dft(convolve_result)
-print("(b) 卷積的離散傅立葉轉換 F(x*g):")
+print("(b) 卷積的離散傅立葉轉換 F(f*g):")
 print(np.around(ft_result, decimal), '\n')
 
-product_ft = ftx * ftg
-print("(c) 分別求離散傅立葉轉換後，再求乘積 F(x)∙F(g):")
+product_ft = ftf * ftg
+print("(c) 分別求離散傅立葉轉換後，再求乘積 F(f)∙F(g):")
 print(np.around(product_ft, decimal), '\n')
+
+inverse_fft = np.fft.ifft(ft_result)
+print("(e) 根據(b)的結果求反離散傅立葉轉換，並與(a)的結果比較:")
+print(np.round(inverse_fft, decimal))
