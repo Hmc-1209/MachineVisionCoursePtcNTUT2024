530bcc36ea2835a304561619b056a33e98f9510b
Danny Ho <113899395+DannyHo1209@users.noreply.github.com>
2024-04-28 22:14:31 +0800

    HW20240410_Fix

--- Fourier HW 20240410/main.py
+++ Fourier HW 20240410/main.py
@@ -1,35 +1,20 @@
 import numpy as np
 
-
-def dft(arr):
-    # Do Fourier Transform
-    length = len(arr)
-    ft_arr = []
-    for k in range(length):
-        sum_value = 0
-        for n in range(length):
-            sum_value += arr[n] * np.exp(-2j * np.pi * k * n / length)
-        ft_arr.append(sum_value)
-    return np.array(ft_arr)
-
-
 decimal = 2
 f = np.array([1, 2, 4, 3, 2, 1, 1])
 g = np.array([1, 1, 1, 1, 1, 1, 1]) * (1/7)
-ftf = dft(f)
-ftg = dft(g)
-print('f的傅立葉轉換：')
-print(np.around(ftf, decimal), '\n')
-print('g的傅立葉轉換：')
-print(np.around(ftg, decimal), '\n')
 
 convolve_result = np.convolve(f, g, mode='full')
 print("(a) 卷積的結果 f*g:")
 print(np.around(convolve_result, decimal), '\n')
 
-ft_result = dft(convolve_result)
+ft_result = np.fft.fft(convolve_result)
 print("(b) 卷積的離散傅立葉轉換 F(f*g):")
 print(np.around(ft_result, decimal), '\n')
+
+# Calculate the padding
+ftf = np.fft.fft(f, n=13)
+ftg = np.fft.fft(g, n=13)
 
 product_ft = ftf * ftg
 print("(c) 分別求離散傅立葉轉換後，再求乘積 F(f)∙F(g):")
